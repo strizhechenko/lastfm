7b45ddc87ff36761382f71eab6d4c6c3572d0d22
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2014-12-12 15:08:51 +0500

    Что было то и пушу

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 
 from flask import Flask
 from flask import render_template
@@ -15,70 +15,24 @@ import time
 app = Flask(__name__)
 host = socket.gethostname()
 
+
 def r_render(tmplt, env):
     return render_template(tmplt, env=env)
 
+
 def get_artists(username):
     if not username or username == '':
-	return []
+        return []
     return user_artists(username)
+
 
 @app.route("/")
 @app.route("/people", methods=['GET', 'POST'])
 def people():
-    user1, user2 = request.args.get('user1'), request.args.get('user2')
-    user1artists = get_artists(user1)
-    if not user1:
-        user1 = ''
-    user2artists = get_artists(user2)
-    if not user2:
-        user2 = ''
-    diff = set(user1artists) & set(user2artists)
-    env = {'user1': user1, 'user2': user2, 'user1artists': user1artists, 'user2artists': user2artists, 'diff': diff}
+    expr = request.args.get('expr')
+    print expr
+    env = {}
     return r_render('people.html', env)
-
-@app.route("/three", methods=['GET', 'POST'])
-def three():
-    user1, user2, user3 = request.args.get('user1'), request.args.get('user2'), request.args.get('user3')
-    user1artists = get_artists(user1)
-    if not user1:
-        user1 = ''
-    user2artists = get_artists(user2)
-    if not user2:
-        user2 = ''
-    user3artists = get_artists(user3)
-    if not user3:
-        user3 = ''
-    diff = set(user1artists) & set(user2artists) & set(user3artists)
-    env = {'diff': diff}
-    return r_render('people.html', env)
-
-
-@app.route("/first", methods=['GET', 'POST'])
-def first():
-    user1, user2 = request.args.get('user1'), request.args.get('user2')
-    user1artists = get_artists(user1)
-    if not user1:
-        user1 = ''
-    user2artists = get_artists(user2)
-    if not user2:
-        user2 = ''
-    diff = set(user1artists) - (set(user1artists) & set(user2artists))
-    env = {'user1': user1, 'user2': user2, 'user1artists': user1artists, 'user2artists': user2artists, 'diff': diff}
-    return r_render('people.html', env)
-
-@app.route("/eval", methods=['GET', 'POST'])
-def eval():
-    user1, user2 = request.args.get('user1'), request.args.get('user2')
-    user1artists = get_artists(user1)
-    if not user1:
-        user1 = ''
-    user2artists = get_artists(user2)
-    if not user2:
-        user2 = ''
-    diff = set(user1artists) - (set(user1artists) & set(user2artists))
-    env = {'user1': user1, 'user2': user2, 'user1artists': user1artists, 'user2artists': user2artists, 'diff': diff}
-    return r_render('eval.html', env)
 
 if __name__ == "__main__":
     app.run()
